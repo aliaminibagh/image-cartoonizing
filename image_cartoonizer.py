0a9fd9f84a1891d8ddbc9d8847f9171d51a7d5a4
Ali Amini Bagh <aliaminibagh@gmail.com>
2020-09-27 23:50:38 +0330

    put bgr instead of rgb

--- image_cartoonizer.py
+++ image_cartoonizer.py
@@ -18,24 +18,25 @@ class Cartoonizer():
 		pass
 
 	def render(self, img_rgb):
-		img_rgb = cv2.imread(img_rgb)
-		img_rgb = cv2.resize(img_rgb, (800,640))
+		img_bgr = cv2.imread(img_rgb)
+		img_bgr = cv2.resize(img_bgr, (640,740))
 
-		num_iter  = 5 #bilateral filtering steps
+		num_iter_bilateral = 5 #bilateral filtering steps
 
 		# Smoothing image
 
-		img_color = img_rgb
+		img_color = img_bgr
+
 		img_color = cv2.pyrDown(img_color)
 
-		for _ in range(num_iter ):
+		for _ in range(num_iter_bilateral):
 			img_color = cv2.bilateralFilter(img_color, d = 9, sigmaColor= 9, sigmaSpace= 7)
 
 		img_color = cv2.pyrUp(img_color)
 
 		# Converting into greyscale & applying the medium blur
 
-		img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
+		img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 		img_blur = cv2.medianBlur(img_gray, ksize= 3)
 
 
@@ -43,15 +44,13 @@ class Cartoonizer():
 		img_edge = cv2.adaptiveThreshold(img_blur, 255,
 										cv2.ADAPTIVE_THRESH_MEAN_C,
 										cv2.THRESH_BINARY, blockSize= 9, C = 2)
-		# cv2.imshow("edge image",img_edge)
-		# cv2.waitKey(0)
 
 		# Again converting back to RGB format
 
-		img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
+		img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2BGR)
 		# cv2.imwrite("edge.png",img_edge)
 		# cv2.imshow("edge RGB", img_edge)
-		cv2.waitKey(0)
+		# cv2.waitKey(0)
 
 		return cv2.bitwise_and(img_color, img_edge)
 
